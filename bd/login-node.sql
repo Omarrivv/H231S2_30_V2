42d49700611bf1c83d54ebd05616be86bd365b97
Omarrivv <omar.rivera@vallegrande.edu.pe>
2023-12-14 12:34:48 -0500

    Agregando Los Nuevos Botones

--- bd/login-node.sql
+++ bd/login-node.sql
@@ -1,22 +1,24 @@
-CREATE DATABASE IF NOT EXISTS EquiposInformaticos;
-USE EquiposInformaticos;
-CREATE TABLE IF NOT EXISTS Equipos (
-    ID INT AUTO_INCREMENT PRIMARY KEY,
-    tipoEquipo VARCHAR(70),
-    Marca VARCHAR(50) NOT NULL,
-    Modelo VARCHAR(50) NOT NULL,
-    NumeroSerie VARCHAR(50) NOT NULL CHECK (NumeroSerie REGEXP '^NS[0-9]{3}$'),
-    EspecificacionesTecnicas TEXT,
-    EstadoMantenimiento VARCHAR(50) NOT NULL
-);
-INSERT INTO Equipos (tipoEquipo, Marca, Modelo, NumeroSerie, EspecificacionesTecnicas, EstadoMantenimiento)
-VALUES ('laptopm', 'Xiaomi', 'gama alta', 'NS123', 'fhhfh fhhf', 'en reparacion'),
-       ('NS456', 'Apple', 'gama baja', 'NS456','gagagga gafaf', 'En reparación'),
-       ('NS789', 'Linux', 'gama media','NS789', 'omar !codeFxRoma', 'Malo');
-UPDATE Equipos
-SET Marca = 'Amazon', EstadoMantenimiento = 'Regular'
-WHERE ID = 2;
-DELETE FROM Equipos
-WHERE ID = 2;
-SELECT * FROM Equipos;
--- DROP DATABASE EquiposInformaticos;
+
+create database registro;
+ use registro;
+
+ create table controlEquipos(
+ id int  primary key auto_increment,
+ Nserie varchar(200),
+ marca varchar(50),
+ modelo varchar(80),
+ Etecnicas varchar(250),
+ descripcion varchar(250),
+ estado varchar(80),
+ fechaCompra date
+ );
+ 
+
+ select * from controlEquipos;
+
+insert into  controlEquipos (Nserie,marca,modelo,Etecnicas,descripcion,estado,fechaCompra) values('202301','hp','core i5','procesador intel','le falta procesador','inactivo','2023-12-01');
+
+update controlEquipos set Nserie='',marca='',modelo='',Etecnicas='',descripcion='',estado='',fechaCompra='' where id='1';
+
+-- delete from controlEquipos where id='1';
+ 
